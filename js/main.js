1ed657ae1f25a49c1d59cc12da4b9e95281f020d
Brian Francisco <ibrianfrancisco@gmail.com>
2017-01-03 00:41:41 -0800

    Game logic complete

--- js/main.js
+++ js/main.js
@@ -1,44 +1,25 @@
 // VARIABLES //
-var player_one = {
-  locationX: 0,
-  locationY: 0,
-  bgColor: 'red',
-  gameChipX: [],
-  gameChipY: []
-}
-
-var player_two = {
-  locationX: 0,
-  locationY: 0,
-  bgColor: 'yellow',
-  gameChipX: [],
-  gameChipY: []
-}
-
-var playerA = new Object(player_one);
-var playerB = new Object(player_two);
-
 var player = 1;
 var $reset = $('#reset');
-var $undo = $('#undo');
 var $board = $('#board');
-var $column = $('#column1');
 var $block = $('div.block');
-var winner = null;
+var $msg = $('#message');
 var board = [[0,0,0,0,0,0,0],
              [0,0,0,0,0,0,0],
              [0,0,0,0,0,0,0],
              [0,0,0,0,0,0,0],
              [0,0,0,0,0,0,0],
              [0,0,0,0,0,0,0]];
-
-var columnCount = [6,6,6,6,6,6];
 var rows = board.length;
 var columns = board[0].length;
 
+
 // EVENT LISTENERS //
 function eventListeners() {
-  $board.on('click', 'div.block', handleClick)
+  $board.on('click', 'div.block', makeMove)
+  $reset.click(function() {
+    location.reload();
+  })
 }
 
 
@@ -47,106 +28,139 @@ function createBoard() {
   for (var i = 0; i < rows; i++) {
     for (var j = 0; j < columns; j++) {
       var block = document.createElement('div');
+      block.id = "loc" + String(i) + String(j);
       block.setAttribute('class', 'block');
       block.setAttribute('row', i);
       block.setAttribute('column', j);
       $board.append(block);
-      if(board[i][j]===1) {
-        block.style.backgroundColor = playerA.bgColor;
-      }
-      if(board[i][j]===2) {
-        block.style.backgroundColor = playerB.bgColor;
-      }
     }
   }
-
-  // var $blocks = $('.block');
-
-  // for (var i=0; i < $blocks.length; i++) {
-  //   $blocks.eq(i).attr('data-value', i);
-  // }
-
-  eventListeners();
 }
 
 
-function makeMove() {
-}
-
-function positionIsTaken(x_pos, y_pos) {
-  var value = board[y_pos][x_pos];
-
-  return value === 0 ? false : true;
-};
-
-function init() {
-  createBoard();
-}
-
-
-function render() {
-}
-
-
-function checkWinner(){
-}
-
-function dropItLikeItsHot(x_pos, y_pos) {
+function dropPiece(x_pos, y_pos) {
   for (var y = 5; y > y_pos; y--) {
     if (board[y][x_pos] === 0) {
       return y;
     }
   }
-
   return y_pos;
 }
 
+
+function positionIsTaken(x_pos, y_pos) {
+  var value = board[y_pos][x_pos];
+  return value === 0 ? false : true;
+}
+
+
 function addValueToBoard(color, x_pos, y_pos) {
-    board[y_pos][x_pos] = color;
-};
+  board[y_pos][x_pos] = color;
+}
 
-// function playTurn(y, turn) {
-//   var coinLocation = columnCount(y);
 
-//   if (coinLocation > 0) {
+function renderLocation(row, column) {
+  var id = "#loc" + String(row) + String(column);
+  var loc = $(id);
+  if(board[row][column]===1) {
+    loc.css('background-color', 'red');
+  }
+  if(board[row][column]===2) {
+    loc.css('background-color', 'yellow');
+  }
+}
 
+
+function checkVertWin() {
+  for (var y = 0; y <= 2; y++) {
+    for (var x = 0; x <= 6; x++) {
+      if (board[y][x] != 0 && board[y][x] == board[y+1][x] && board[y][x] == board[y+2][x] && board[y][x] == board[y+3][x]) {
+        return (player);
+      }
+   }
+  }
+}
+
+
+function checkHorizWin() {
+  for (var y = 0; y <= 5; y++) {
+    for (var x = 0; x <= 3; x++) {
+      if (board[y][x] != 0 && board[y][x] == board[y][x+1] && board[y][x] == board[y][x+2] && board[y][x] == board[y][x+3]) {
+        return (player);
+      }
+    }
+  }
+}
+
+
+function checkDiagDownWin() {
+  for (var y = 0; y <= 2; y++) {
+    for (var x = 0; x <= 3; x++) {
+      if (board[y][x] != 0 && board[y][x] == board[y+1][x+1] && board[y][x] == board[y+2][x+2] && board[y][x] == board[y+3][x+3]) {
+        return (player);
+      }
+    }
+  }
+}
+
+
+function checkDiagUpWin() {
+  for (var y = 3; y <= 5; y++) {
+    for (var x = 0; x <= 3; x++) {
+      if (board[y][x] != 0 && board[y][x] == board[y-1][x+1] && board[y][x] == board[y-2][x+2] && board[y][x] == board[y-3][x+3]) {
+        return (player);
+      }
+    }
+  }
+}
+
+
+// function gameIsDraw() {
+//   for (var y = 0; y < rows; y++) {
+//     for (var x = 0; x < columns; x++) {
+//       return (board[y][x] > 0) ? true : false;
+//     }
 //   }
 // }
 
-// function eventHandler(evt) {
-//   var tempX;
-//   var tempY;
-//   var tempTurn = player;
 
-//   if(player===0) {
-//     tempY = playerA.locationY;
-//   }
-// }
+function winner(play) {
+  var declarePlayer = (play === 1) ? "Player 1 Wins!" : "Player 2 Wins!";
+  $msg.html(declarePlayer);
+  $board.unbind('click');
+  return
+}
 
 
-function handleClick() {
+function updateMsg(currentPlayer) {
+  $msg.html("It is player " + currentPlayer + "'s turn");
+}
+
+
+function makeMove() {
   var y_pos = $(this).attr('row');
   var x_pos = $(this).attr('column');
-
-  // console.log(x_pos, y_pos);
-  y_pos = dropItLikeItsHot(x_pos, y_pos);
-  // console.log(y_pos);
-
+  y_pos = dropPiece(x_pos, y_pos);
   if (positionIsTaken(x_pos, y_pos)) {
     return;
   }
-
-
   addValueToBoard(player, x_pos, y_pos);
-  // createBoard();
-  render();
-
+  renderLocation(y_pos, x_pos);
+  if (checkHorizWin() || checkVertWin() || checkDiagUpWin() || checkDiagDownWin()) {
+    winner(player);
+    return;
+  }
   player = (player === 1) ? 2 : 1;
-  checkWinner();
+  updateMsg(player);
+  // gameIsDraw();
+  // if (gameIsDraw === true) {
+  //   $msg.html('Tie Game!');
+  // }
 }
 
-createBoard();
+function init() {
+  createBoard();
+  eventListeners();
+}
 
-
-// var y_pos = $('#board .column').index($(this).closest('.block'));
-// var x_pos = $(this).closest('.block').find('.block').index($(this).closest('.block'));
+init();
